394d17f705ed35652a9dc20251a5c299afb6e6cd
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-02-01 14:20:47 -0700

    updated banner

--- natster/banner.go
+++ natster/banner.go
@@ -4,12 +4,40 @@ package main
 
 const (
 	Banner = `
-
-	 ▐ ▄  ▄▄▄· ▄▄▄▄▄.▄▄ · ▄▄▄▄▄▄▄▄ .▄▄▄  
-	•█▌▐█▐█ ▀█ •██  ▐█ ▀. •██  ▀▄.▀·▀▄ █·
-	▐█▐▐▌▄█▀▀█  ▐█.▪▄▀▀▀█▄ ▐█.▪▐▀▀▪▄▐▀▀▄ 
-	██▐█▌▐█ ▪▐▌ ▐█▌·▐█▄▪▐█ ▐█▌·▐█▄▄▌▐█•█▌
-	▀▀ █▪ ▀  ▀  ▀▀▀  ▀▀▀▀  ▀▀▀  ▀▀▀ .▀  ▀
-	
+                           XXXXXX                            
+                     XXXXXXXXXXXXXXXXXXX                     
+                 xXXXXXXXXXXXXXXXXXXXXXXXXX                  
+               XXXXXXXXXXx         xXXXXXXXXXX               
+             XXXXXXXX                   XXXXXXXX             
+            XXXXXX                         XXXXXX            
+          XXXXXX                             XXXXXX          
+       XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX       
+       XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX       
+       XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX       
+       XXXXx                                     XXXXX       
+       XXXXx                                     XXXXX       
+    XXXXXXXx                                     XXXXXXXX    
+   XXXXXXXXx    ++++++++++              +++++    XXXXXXXXX   
+  XXXXXXXXXx    ++++++++++++            +++++    XXXXXXXXXX  
+ XXXXXXXXXXx    +++++ ++++++++          +++++    XXXXXXXXXXX 
+ XXXXXXXXXXx    +++++   +++++++         +++++    XXXXXXXXXXX 
+ XXXXXXXXXXx    +++++     +++++++       +++++    XXXXXXXXXXX 
+ XXXXXXXXXXx    +++++       +++++++     +++++    XXXXXXXXXXX 
+  XXXXXXXXXx    +++++         +++++++   +++++    XXXXXXXXXX  
+    XXXXXXXx    +++++           +++++++ +++++    XXXXXXXXX   
+      XXXXXx    +++++             +++++++++++    XXXXXX      
+       XXXXx    +++++               +++++++++    XXXXX       
+       XXXXx                                     XXXXX       
+       XXXXx                                     XXXXX       
+       XXXXx                                     XXXXX       
+       XXXXXXXXXXXXXXXXXXXXXX      xXXXXXXXXXXXXXXXXXX       
+       XXXXXXXXXXXXXXXXXXXXXXXX    +XXXXXXXXXXXXXXXXXX       
+                          XXXXXXX  +XXXX                     
+                            xXXXXXXxXXXX                     
+                              +XXXXXXXXX                     
+                                 XXXXXXX                     
+                                   XXXXX                     
+                                     XXX                     
+                                       X
 `
 )
