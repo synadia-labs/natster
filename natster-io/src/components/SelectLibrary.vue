5c194eb760bb7db3a86c25b30bed02875f6b9fc6
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-02-29 11:04:03 -0700

    kevin words better than i

--- natster-io/src/components/SelectLibrary.vue
+++ natster-io/src/components/SelectLibrary.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="relative pt-10">
-    <div
-      class="relative h-80 overflow-hidden md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2"
-    >
-      <img
-        class="h-full w-full "
-        :src="natsterImg"
-        alt=""
-      />
+    <div class="relative h-80 overflow-hidden md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
+      <img class="h-full w-full" :src="natsterImg" alt="" />
     </div>
     <div class="relative mx-auto max-w-7xl py-24 sm:py-32 lg:px-8 lg:py-40">
       <div class="pl-6 pr-6 md:ml-auto md:w-2/3 md:pl-16 lg:w-1/2 lg:pl-24 lg:pr-0 xl:pl-32">
@@ -16,9 +10,8 @@
           Choose from one of your shared catalogs
         </p>
         <p class="mt-6 text-base leading-7">
-        Any catalog that has been shared and you have imported will show up on in 
-        the left sidebar. You can select a catalog to view the files that catalog 
-        has chosen to share.
+          Any catalog that has been shared and you have imported will show up in the left sidebar.
+          You can select a catalog to view the files that catalog.
         </p>
       </div>
     </div>
