37edba870242f508f1aef576a5cb9e14d5b7ce85
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-03-12 11:18:55 -0600

    support images and plain text

--- natster-io/src/components/File.vue
+++ natster-io/src/components/File.vue
@@ -1,40 +1,24 @@
 <template>
   <TransitionRoot as="template" :show="show">
     <Dialog as="div" class="relative z-10" @close="close()">
-      <TransitionChild
-        as="template"
-        enter="ease-out duration-300"
-        enter-from="opacity-0"
-        enter-to="opacity-100"
-        leave="ease-in duration-200"
-        leave-from="opacity-100"
-        leave-to="opacity-0"
-      >
+      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
+        leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
       </TransitionChild>
 
       <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div
-          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
-        >
-          <TransitionChild
-            as="template"
-            enter="ease-out duration-300"
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <TransitionChild as="template" enter="ease-out duration-300"
             enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enter-to="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
+            enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
             leave-from="opacity-100 translate-y-0 sm:scale-100"
-            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          >
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
             <DialogPanel
-              class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
-            >
+              class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
               <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
-                <button
-                  type="button"
+                <button type="button"
                   class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  @click="close()"
-                >
+                  @click="close()">
                   <span class="sr-only">Close</span>
                   <XMarkIcon class="h-6 w-6" aria-hidden="true" />
                 </button>
@@ -42,8 +26,7 @@
               <div class="sm:flex sm:items-start">
                 <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                   <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">
-                    {{ catalog.name }} | {{ title }}</DialogTitle
-                  >
+                    {{ catalog.name }} | {{ title }}</DialogTitle>
                   <div class="mt-2">
                     <p v-if="!!body" class="text-sm text-gray-500">
                       {{ body }}
@@ -52,44 +35,28 @@
                 </div>
               </div>
               <div class="relative">
-                <VueSpinnerAudio 
-                  v-if="loading"
-                  size="80"
-                  class="loading-spinner"
-                />
+                <VueSpinnerAudio v-if="loading" size="80" class="loading-spinner" />
 
-                <video
-                  v-if="!!mediaUrl && mimeType.toLowerCase() == 'video/mp4'"
-                  v-show="!loading"
-                  id="video"
-                  :type="mimeType"
-                  :src="mediaUrl"
-                  width="640"
-                  height="360"
-                  autoplay
-                  controls
-                ></video>
-                
-                <AudioPlayer
-                  v-if="!!mediaUrl && mimeType.toLowerCase() == 'audio/mpeg'"
-                  v-show="!loading"
-                  @loadedmetadata="playAudio(e)"
-                  :option="
-                    getAudioOptions(
-                      mediaUrl,
-                      description == '' ? title : description,
-                      catalog.image
-                    )
-                  "
-                  :title="title"
-                />
+                <video v-if="!!mediaUrl && mimeType.toLowerCase() == 'video/mp4'" v-show="!loading" id="video"
+                  :type="mimeType" :src="mediaUrl" width="640" height="360" autoplay controls></video>
+                <AudioPlayer :option="getAudioOptions(
+    mediaUrl,
+    description == '' ? title : description,
+    catalog.image
+  )
+    " v-else-if="!!mediaUrl && mimeType.toLowerCase() == 'audio/mpeg'" v-show="!loading" @loadedmetadata="playAudio(e)"
+                  :title="title" />
+                <img v-else-if="blob != null && mimeType.toLowerCase() === 'image/png'" :src="blobData"
+                  v-show="!loading" :alt="title" />
+                <p v-else-if="blob != null && mimeType.toLowerCase() === 'text/plain'" v-show="!loading">
+                  {{ blobData }}
+                </p>
+                <p v-else v-show="!loading">Error displaying media</p>
               </div>
               <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                <button
-                  type="button"
+                <button type="button"
                   class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                  @click="close()"
-                >
+                  @click="close()">
                   Close
                 </button>
               </div>
@@ -102,7 +69,7 @@
 </template>
 
 <script setup lang="ts">
-import { watch } from 'vue'
+import { ref, watch } from 'vue'
 import { storeToRefs } from 'pinia'
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import { XMarkIcon } from '@heroicons/vue/24/outline'
@@ -110,16 +77,33 @@ import { VueSpinnerAudio } from 'vue3-spinners'
 
 import AudioPlayer from 'vue3-audio-player'
 import 'vue3-audio-player/dist/style.css'
-
 import { fileStore } from '../stores/file'
 const fStore = fileStore()
-const { body, title, show, loading, mimeType, mediaUrl, catalog, description } = storeToRefs(fStore)
+const { body, title, show, loading, mimeType, mediaUrl, catalog, description, blob } = storeToRefs(fStore)
+
+const blobData = ref(null)
 
 function close() {
   console.log('closing file view')
   fStore.show = false
   fStore.reset()
 }
+
+watch(blob, async (newVal, oldVal) => {
+  console.log('blob changed', newVal, mimeType.value)
+
+  if (newVal != null && mimeType.value == 'image/png') {
+    var urlCreator = window.URL || window.webkitURL;
+    blobData.value = urlCreator.createObjectURL(newVal);
+  } else if (newVal != null && mimeType.value == 'text/plain') {
+    await newVal.text()
+      .then((text) => {
+        blobData.value = text
+      })
+  } else {
+    blobData.value = null
+  }
+})
 
 watch(mimeType, (newVal, oldVal) => {
   console.log(`mime type changed... ${newVal}`)
@@ -149,7 +133,7 @@ watch(mediaUrl, (newVal, oldVal) => {
             fStore.loading = false
           }
         })
-      } catch (e) {}
+      } catch (e) { }
 
       try {
         document.querySelector('video').addEventListener('play', (event) => {
@@ -157,7 +141,7 @@ watch(mediaUrl, (newVal, oldVal) => {
             fStore.loading = false
           }
         })
-      } catch (e) {}
+      } catch (e) { }
     }, 50)
   }
 })
