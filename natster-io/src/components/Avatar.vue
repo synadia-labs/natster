1c71a38c32ff61ba47527162d590481c397e1b4f
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-03-12 15:14:56 -0600

    adds docs button (#97)

--- natster-io/src/components/Avatar.vue
+++ natster-io/src/components/Avatar.vue
@@ -19,7 +19,13 @@
         enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75"
         leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
         <MenuItems
-          class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none bottom-full">
+          class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none bottom-full">
+          <div class="py-1">
+            <MenuItem v-slot="{ active }">
+            <a href="https://docs.natster.io"
+              :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Docs</a>
+            </MenuItem>
+          </div>
           <div class="py-1">
             <MenuItem v-slot="{ active }">
             <button type="submit" @click.prevent="signout" :class="[
