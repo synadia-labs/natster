135ed9673f86789a6dddd98696ebf7fd0d6f78a3
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-03-13 10:22:59 -0600

    shows spinner in active download (#99)

--- natster-io/src/App.vue
+++ natster-io/src/App.vue
@@ -1,10 +1,7 @@
 <template>
-  <VueSpinnerAudio 
-    v-if="isLoading"
-    size="160"
-    class="loading-spinner"
-  />
+  <VueSpinnerAudio v-if="isLoading" size="160" class="loading-spinner" />
   <div v-else>
+    <VueSpinnerTail v-if="activeDownload" size="160" class="downloading-spinner" />
     <AuthView v-if="isAuthenticated">
       <router-view />
     </AuthView>
@@ -14,15 +11,31 @@
 
 <script setup lang="ts">
 import { useAuth0 } from '@auth0/auth0-vue'
-import AuthView from './views/AuthView.vue'
-import { VueSpinnerAudio } from 'vue3-spinners'
+import { storeToRefs } from 'pinia'
 
+import AuthView from './views/AuthView.vue'
+import { catalogStore } from './stores/catalog'
+const { activeDownload } = storeToRefs(catalogStore())
+
+
+import { VueSpinnerAudio, VueSpinnerTail } from 'vue3-spinners'
 const { isAuthenticated, isLoading } = useAuth0()
+
 </script>
 
 <style>
 .loading-spinner {
   color: #45c320;
-  margin: 10% auto;
+  margin: auto auto;
+}
+
+.downloading-spinner {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-top: -50px;
+  margin-left: -100px;
+  color: #45c320;
+  z-index: 50;
 }
 </style>
